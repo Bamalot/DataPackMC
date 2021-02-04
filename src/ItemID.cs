12e69568a314f942d0e95a1703535bd8b9689d34
Blake Hardrick <bhardrick04@gmail.com>
2021-02-04 15:39:25 -0500

    Reorganize project

---
+++ src/ItemID.cs
@@ -0,0 +1,15 @@
+
+namespace DataPackMC
+{
+    public static class ItemID
+    {
+        public const string AcaciaBoat = "acacia_boat";
+        public const string Apple = "apple";
+        public const string ArmorStand = "armor_stand";
+        public const string Arrow = "arrow";
+        public const string BakedPotato = "baked_potato";
+        public const string BatSpawnEgg = "bat_spawn_egg";
+        public const string BeeSpawnEgg = "bee_spawn_egg";
+        public const string RawBeef = "beef";
+    }
+}
